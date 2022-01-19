bfc2e182ff89c06c2500da4ecd76bfe5c970fb98
Javier <javier@eskura21.com>
2022-01-19 19:22:46 +0100

    Componente de listado de películas

---
+++ src/app/_componentes/peliculas/peliculas.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Pelicula } from 'src/app/_modelos/pelicula';
+import { PeliculaService } from 'src/app/_servicios/pelicula.service';
+
+@Component({
+  selector: 'app-peliculas',
+  templateUrl: './peliculas.component.html',
+  styleUrls: ['./peliculas.component.css']
+})
+export class PeliculasComponent implements OnInit {
+
+  peliculas: Pelicula[] = [];
+
+  constructor(private peliculaService: PeliculaService) { }
+
+  ngOnInit(): void {
+    this.peliculaService.obtenerPeliculas().subscribe(pelis => this.peliculas = pelis);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { PeliculasComponent } from './_componentes/peliculas/peliculas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PeliculasComponent
   ],
   imports: [
     BrowserModule,
